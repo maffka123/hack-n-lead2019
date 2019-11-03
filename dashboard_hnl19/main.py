cba4f72d4336d8f249eb6f02547198161b9565a3
maffka123 <mstihomirova@gmail.com>
2019-11-03 10:21:21 +0100

    presentation

--- dashboard_hnl19/main.py
+++ dashboard_hnl19/main.py
@@ -1,49 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from random import random
-from macros import embed
-from bokeh.layouts import column
 from bokeh.models import Button
-from bokeh.palettes import RdYlBu3
 from bokeh.plotting import figure, curdoc
+from bokeh.layouts import gridplot
+import numpy as np
+from bokeh.layouts import widgetbox
+from bokeh.models import CustomJS, TextInput, Paragraph
 
-# create a plot and style its properties
-p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
-p.border_fill_color = 'black'
-p.background_fill_color = 'black'
-p.outline_line_color = None
-p.grid.grid_line_color = None
+#plots = [figure(title = 'Styles Demo {i}'.format(i = i + 1), plot_width = 200, plot_height = 200, tools = '') for i in range(9)]
+#[plot.line(np.arange(10), np.random.random(10)) for plot in plots]
 
-# add a text renderer to our plot (no data yet)
-r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
-           text_baseline="middle", text_align="center")
+# CALLBACKS
+def callback_print(text_banner=text_banner):
+    user_input = str(cb_obj.value)
+    welcome_message = 'You have selected: ' + user_input
+    text_banner.text = welcome_message
 
-i = 0
+# USER INTERACTIONS
+text_input = TextInput(value="", title="Enter row number:",             
+callback=CustomJS.from_py_func(callback_print))
 
-ds = r.data_source
-
-# create a callback that will add a number in a random location
-def callback():
-    global i
-
-    # BEST PRACTICE --- update .data in one step with a new dict
-    new_data = dict()
-    new_data['x'] = ds.data['x'] + [random()*70 + 15]
-    new_data['y'] = ds.data['y'] + [random()*70 + 15]
-    new_data['text_color'] = ds.data['text_color'] + [RdYlBu3[i%3]]
-    new_data['text'] = ds.data['text'] + [str(i)]
-    ds.data = new_data
-
-    i = i + 1
-
-# add a button widget and configure with the call back
-button = Button(label="Press Me")
-button.on_click(callback)
-
-# put the button and plot in a layout and add to the document
-curdoc().add_root(column(button, p))
-
-
-
-# ! bokeh serve --show main.ipynb
+button_bokeh = Button(label = "Submit your complain", css_classes = ['custom_button_bokeh'])
+curdoc().add_root(text_input)
+curdoc().add_root(button_bokeh)
